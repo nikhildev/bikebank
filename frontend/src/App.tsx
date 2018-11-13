4540d415f57f8f5151b434ac47186532d1dd44dd
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2018-11-13 20:59:50 +0200

    AppToolbar styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+
 import './App.css';
 
-import AppToolbar from './components/AppToolbar';
+import AppToolbar from './components/AppToolbar/AppToolbar';
 import SearchPage from "./containers/SearchPage/SearchPage";
 
 class App extends React.Component {
   public render() {
     return (
-      <div className="App">
-        <AppToolbar
-          appTitle={'Bike Bank'}/>
-        <SearchPage />
-      </div>
+      <BrowserRouter>
+        <div className="App">
+          <AppToolbar
+            appTitle={'Bike Bank'}
+          />
+          <SearchPage />
+        </div>
+      </BrowserRouter>
     );
   }
 }
---
+++ frontend/src/components/AppToolbar/AppToolbar.tsx
@@ -0,0 +1,38 @@
+import * as React from 'react';
+import {
+  Link,
+} from 'react-router-dom';
+
+import './AppToolbar.css';
+
+interface IProps {
+  appTitle: string,
+}
+
+class AppToolbar extends React.Component<IProps> {
+  constructor(props: IProps) {
+    super(props);
+  }
+
+  public render() {
+    return (
+      <div id="AppToolbar">
+        <div id="AppTitle">{this.props.appTitle}</div>
+        <nav id="AppNavBar">
+          <Link
+            to="/"
+            className={'active'}
+          >
+            Home
+          </Link>
+          <Link to="/">Dashboard</Link>
+        </nav>
+        <div id="ToolbarActions">
+          Actions
+        </div>
+      </div>
+    )
+  }
+}
+
+export default AppToolbar;
