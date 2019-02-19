42b25cd4380f8e27694e692734b5edd35d36a256
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2019-02-19 21:45:03 +0200

    Rewriting form with withFormik HOC

--- frontend/src/pages/RegisterBikePage/RegisterBikePage.tsx
+++ frontend/src/pages/RegisterBikePage/RegisterBikePage.tsx
@@ -1,12 +1,60 @@
-import { Formik } from 'formik';
+import { FormikProps, FormikErrors, withFormik, Form, Field } from 'formik';
 import * as React from 'react';
 
-import { IBike } from '../../types/bike';
+import { IBike, BIKE_STATUS } from '../../types/bike';
 
 interface IState {
   form: any;
   error: any;
 }
+
+const INNER_FORM = (props: FormikProps<IBike>) => {
+  const { touched, errors, isSubmitting } = props;
+
+  return (
+    <Form>
+      <Field type="input" name="make" />
+      {touched.make && errors.make && <div>{errors.make}</div>}
+
+      <button type="submit" disabled={isSubmitting}>
+        Submit
+      </button>
+    </Form>
+  );
+}
+
+const MyForm = withFormik<IBike, IBike>({
+  // Transform outer props into form values
+  mapPropsToValues: props => {
+    return {
+      accessories: props.accessories || '',
+      color: props.color || '',
+      description: props.description || '',
+      id: props.id || '',
+      make: props.make || '',
+      model: props.model || '',
+      ownerId: props.ownerId || '',
+      purchaseDate: props.purchaseDate || '',
+      serial: props.serial || '',
+      status: props.status || BIKE_STATUS.IN_POSSESSION,
+    };
+  },
+
+  // Add a custom validation function (this can be async too!)
+  validate: (values: IBike) => {
+    let errors: FormikErrors<IBike> = {};
+    if (!values.make || !values.make.length) {
+      errors.make = 'Required';
+    }
+    return errors;
+  },
+
+  handleSubmit: values => {
+    console.log(values);
+    
+    // do submitting things
+  },
+})(INNER_FORM);
 
 const INITIAL_FORM: IBike = {
   accessories: '',
@@ -18,18 +66,15 @@ const INITIAL_FORM: IBike = {
   ownerId: '',
   purchaseDate: '',
   serial: '',
-  status: 0,
+  status: BIKE_STATUS.IN_POSSESSION,
 }
 
 class RegisterBikePage extends React.Component<{}, IState> {
-  constructor(props: any) {
-    super(props);
 
-    this.state = {
-      error: null,
-      form: INITIAL_FORM,
-    };
-  }
+  readonly state: IState = {
+    error: null,
+    form: INITIAL_FORM,
+  };
 
   public render() {
     return (
@@ -41,36 +86,7 @@ class RegisterBikePage extends React.Component<{}, IState> {
         }}
       >
         <h1>Register a new bike</h1>
-        <Formik
-          initialValues={this.state.form}
-          validate={this.validateForm}
-          onSubmit={this.handleSubmit}
-        >
-          {({
-            values,
-            errors,
-            touched,
-            handleChange,
-            handleBlur,
-            handleSubmit,
-            isSubmitting,
-            /* and other goodies */
-          }) => (
-            <form onSubmit={handleSubmit}>
-              <input
-                type="text"
-                name="serial"
-                onChange={handleChange}
-                onBlur={handleBlur}
-                value={values.serial}
-              />
-              {errors.errors && touched.email && errors.email}
-              <button type="submit" disabled={isSubmitting}>
-                Submit
-              </button>
-            </form>
-          )}
-        </Formik>
+        <MyForm id="" serial="" status={0} />
       </main>
     );
   }
@@ -81,20 +97,20 @@ class RegisterBikePage extends React.Component<{}, IState> {
     
   }
 
-  private validateForm = (values: IBike) => {
-    // values => {
-    //   let errors = {};
-    //   if (!values.email) {
-    //     errors.email = 'Required';
-    //   } else if (
-    //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
-    //   ) {
-    //     errors.email = 'Invalid email address';
-    //   }
-    //   return errors;
-    // }
-    // return true;
-  }
+  // private validateForm = (values: IBike) => {
+  //   // values => {
+  //   //   let errors = {};
+  //   //   if (!values.email) {
+  //   //     errors.email = 'Required';
+  //   //   } else if (
+  //   //     !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(values.email)
+  //   //   ) {
+  //   //     errors.email = 'Invalid email address';
+  //   //   }
+  //   //   return errors;
+  //   // }
+  //   // return true;
+  // }
 
 }
 
