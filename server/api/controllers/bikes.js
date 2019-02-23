3218a4e27e921d7dbe56fbe5eb1f1e48a84e34e2
Nikhil Dev Chunchu <nikhil@nikhildev.com>
2019-02-24 01:29:10 +0200

    Converted promise based callbacks to await. Looks neat now. Gotta hit the sack!

--- server/api/controllers/bikes.js
+++ server/api/controllers/bikes.js
@@ -23,68 +23,78 @@ function validateBike(bikeObject) {
   };
 }
 
-function createBike(req, res) {
+async function createBike(req, res) {
   const bikeRequestObject = req.swagger.params.bike.value;
   const bikeValidationResult = validateBike(bikeRequestObject);
 
   if (bikeValidationResult.error) {
     res.status(400).json(bikeValidationResult);
-  } else {
-    const newBikeId = uuid.v4();
-    const bikeObject = {
-      ...bikeRequestObject,
-      id: newBikeId,
-    };
-    const userRef = Users.doc(req.user.uid);
-    const query = Bikes.where('serial', '==', bikeRequestObject.serial).get()
-      .then(snapshot => {
-        if (!snapshot.empty) {
-          res.status(409).json({
-            message: `Bike already registered`,
-          });
-        } else {
-          userRef.get()
-            .then(async snapshot => {
-              if (snapshot.exists) {
-                const bikes = snapshot.data().bikes || [];
-      
-                const filteredBikes = bikes.filter(bike => bike.serial === bikeRequestObject.serial);
-      
-                if (filteredBikes.length) {
-                  res.status(409).json({
-                    message: 'Bike already exists in user profile',
-                  });
-                } else {
-                  Bikes.doc(newBikeId).set(bikeObject).then(ref => {
-                    bikes.push(newBikeId);
-                    
-                    userRef.update({
-                      bikes,
-                    }).then(updateResponse => {
-                      console.log(`CREATED_BIKE ${JSON.stringify(bikeObject)}`);
-                      res.json(bikeObject);
-                    }).catch(updateError => {
-                      console.error(updateError);
-                      res.status(500).json({
-                        message: 'An error occurred while updating the user with then new bike information',
-                      });
-                    });
-                  }).catch((error) => {
-                    console.error(error);
-                    res.status(500).json({
-                      message: 'An error occurred while saving the new bike information',
-                    });
-                  });
-                }
-              }
-            })
-            .catch(error => {
-              console.error(error);
-            });
-        }
-      });
-  
+  }
 
+  const newBikeId = uuid.v4();
+  const bikeObject = {
+    ...bikeRequestObject,
+    id: newBikeId,
+  };
+  const userRef = Users.doc(req.user.uid);
+
+  // Try fetching bike with the serial number in the request
+  try {
+    const bikesSnapshot = await Bikes.where('serial', '==', bikeRequestObject.serial).get();
+    if (!bikesSnapshot.empty) {
+      res.status(409).json({
+        message: `Bike already registered`,
+      });
+    }
+  } catch {
+    res.status(500).json({
+      message: 'An unknown error has occurred',
+    })
+  }
+
+  let userSnapshot;
+
+  // Get user information
+  try {
+    userSnapshot = await userRef.get();
+  } catch {
+    res.status(500).json({
+      message: 'An error occurred fetching the user information',
+    });
+  }
+
+  const bikes = userSnapshot.data().bikes || [];
+  const filteredBikes = bikes.filter(bike => bike.serial === bikeRequestObject.serial);
+
+  if (filteredBikes.length) {
+    res.status(409).json({
+      message: 'Bike already exists in user profile',
+    });
+  }
+
+  // Insert new bike info into Bikes collection
+  try {
+    const newBikeSnapshot = await Bikes.doc(newBikeId).set(bikeObject);
+  } catch {
+    res.status(500).json({
+      message: 'An error occurred while saving the new bike information',
+    });
+  }
+
+  bikes.push(newBikeId);
+
+  // Update user document with newly created bike id
+  try {
+    const userUpdateSnapshot = await userRef.update({
+      bikes,
+    });
+    console.log(`CREATED_BIKE ${JSON.stringify(bikeObject)}`);
+    res.json(bikeObject);
+  } catch(error) {
+    console.error(error);
+    res.status(500).json({
+      message: 'An error occurred while updating the user with then new bike information',
+    });
   }
 
 }
